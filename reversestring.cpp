5adb70d9533d3b469d492268061f350e01cbf6a5
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2022-10-01 18:03:15 +0530

    Update reversestring.cpp
    reverse string using stack

--- reversestring.cpp
+++ reversestring.cpp
@@ -9,3 +9,30 @@ int main()
    
    cout<<str;
 }
+
+
+//2nd method - Reverse string using stack
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string str;
+    cin>>str;
+    stack<char> s;
+    for(int i=0;i<str.length();i++){
+        char ch=str[i];
+        s.push(ch);
+}
+
+    string ans="";
+    while(!s.empty()){
+        char ch= s.top();
+        ans.push_back(ch);
+
+        s.pop();
+}
+    cout<<ans;
+    return 0;
+}
